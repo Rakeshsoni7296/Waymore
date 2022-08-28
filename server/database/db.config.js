6bf4447dc68977d3a9bc6e33904489cbefc50bd1
Rakesh Soni <welfaredeanstudent@gmail.com>
2022-08-28 12:45:13 +0530

    slight change

--- server/database/db.config.js
+++ server/database/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: "localhost",
   user: "root",
-  password: "vzaykutmbjbjguydfudhfugydhvuydfhkuybudkfhvuydshbufvhudsfh",
-  db: "waymore_blogfkjhsufgydufguysdfhuydshgyfudfsyugvbyudsgyvguyhiushrfvuygrgf",
+  password: "vzaykutmbjiyufdvydsgufhvdsfvbdfvdyhvuhsdgvoifdsjivufdvgyvhdhv",
+  db: "waymore_bloglivhdysufvgusdyvgfsdcvhdgfvdsfuvdsbfugfldijvdsfhiguhdilfugvuydui",
   dialect: "mysql",
 };
